f853b9aee974308ebe9d4f90c01be73920881af2
rajurao <rajurao1107@gmail.com>
2025-11-17 18:22:32 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: ["https://quickscan-nine.vercel.app"],
+    credentials: true,
+  })
+);
+
 
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
