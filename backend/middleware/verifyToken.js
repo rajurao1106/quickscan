a84ab6e44fb2fe977366f758ef980aec02acf49e
rajurao <rajurao1107@gmail.com>
2025-11-17 18:59:43 +0530

    commit

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,17 +1,14 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req, res, next) => {
-	const token = req.cookies.token;
-	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+export const generateTokenAndSetCookie = (res, userId) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "7d",
+	});
 
-		if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
-
-		req.userId = decoded.userId;
-		next();
-	} catch (error) {
-		console.log("Error in verifyToken ", error);
-		return res.status(500).json({ success: false, message: "Server error" });
-	}
+	res.cookie("token", token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production", // required on Vercel
+		sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // REQUIRED for cross-domain
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	});
 };
