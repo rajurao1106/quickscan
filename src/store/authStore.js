e87887db919590ec68b9d44b85d7c3b5a211669f
rajurao <rajurao1107@gmail.com>
2025-11-21 09:33:53 +0530

    commit

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,10 +2,7 @@ import React, { useEffect } from "react";
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/auth"
-    : "https://quickscan.onrender.com/api/auth";
+const API_URL = "https://quickscan.onrender.com/api/auth";
 
 
 axios.defaults.withCredentials = true;
